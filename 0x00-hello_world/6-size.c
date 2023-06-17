30f0a1cec62e4a798d44644176c9b043d3c64c7f
OUTMANE-BEN-MOUSSA <outmaneben1999@gmail.com>
2023-06-17 04:12:53 -0700

    6th commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-*main - Print Sizes
-*
-*Return : Always 0 (success)
-*/
+ * main - Print Sizes
+ *
+ * Return : Always 0 (success)
+ */
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
