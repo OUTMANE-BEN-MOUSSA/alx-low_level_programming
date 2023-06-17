a518ae7a7d37a300a66e13cb98566daeb09cfbf4
OUTMANE-BEN-MOUSSA <outmaneben1999@gmail.com>
2023-06-17 04:08:01 -0700

    6th commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 
 /**
- *main - The Entry Point
- *
- *Return : Always 0 (success)
- */
+*main - Print Sizes
+*
+*Return : Always 0 (success)
+*/
 int main(void)
 {
-	char c;
-	int i;
-	long int li;
-	long long int lli;
-	float f;
-	
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
-	printf("Size float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
