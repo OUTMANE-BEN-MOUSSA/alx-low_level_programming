8121df9044d085537079a5c7451b3d07b6696d15
OUTMANE-BEN-MOUSSA <outmaneben1999@gmail.com>
2023-08-01 11:43:31 -0700

    5th push

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint_safe - This function prints the elementsin a  list
+ * @head: A pointer to listint_t structure
+ * Return: The number of nodes or exits with 98 on failure
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes_number = 0;
+	const listint_t *node1 = head, *node2 = head;
+
+	if (head == NULL)
+		exit(98);
+
+	while (node1 && node2 && node2->next && head)
+	{
+		node1 = node1->next;
+		node2 = node2->next->next;
+		if (node1 == node2)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			exit(98);
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nodes_number++;
+	}
+	head = NULL;
+	return (nodes_number);
+}
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - This function finds the loop in a list
+ * @head: A pointer to listint_t structure
+ * Return: The address of the node where the loop start, or NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *node1 = head, *node2 = head;
+
+	while (node1 && node2 && node2->next)
+	{
+		node1 = node1->next;
+		node2 = node2->next->next;
+		if (node1 == node2)
+		{
+			return (node1);
+		}
+	}
+	return (NULL);
+}
