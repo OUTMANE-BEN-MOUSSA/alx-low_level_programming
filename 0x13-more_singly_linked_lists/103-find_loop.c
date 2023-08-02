004b8125f9984e28df48910a522d1d1187ecf338
OUTMANE-BEN-MOUSSA <outmaneben1999@gmail.com>
2023-08-02 08:54:13 -0700

    6th push

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -3,19 +3,19 @@
 /**
  * find_listint_loop - This function finds the loop in a list
  * @head: A pointer to listint_t structure
- * Return: The address of node where the loop start or NULL
+ * Return: The address of the node where the loop start or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *node1 = head, *node2 = head;
+	listint_t *o = head, *t = head;
 
-	while (node1 && node2 && node2->next)
+	while (o && t && t->next)
 	{
-		node1 = node1->next;
-		node2 = node2->next->next;
-		if (node1 == node2)
+		o = o->next;
+		t = t->next->next;
+		if (o == t)
 		{
-			return (node1);
+			return (o);
 		}
 	}
 	return (NULL);
