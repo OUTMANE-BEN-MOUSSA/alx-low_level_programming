13ef2c8d4eec2df42c324c97c8f34b6abcad4d23
OUTMANE-BEN-MOUSSA <outmaneben1999@gmail.com>
2023-09-10 10:53:42 -0700

    2nd commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - This function that the length of a string.
+ * @s: An input string to printing
+ * Return: The length of the string as integer
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+
